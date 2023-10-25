1469445e39bd623ab7fdfdbc388bf89c8bbfa3c9
Joseph Ellis <josephellis@Josephs-MacBook-Air.local>
2023-10-25 13:08:13 -0500

    finished

--- frontend/index.js
+++ frontend/index.js
@@ -1,13 +1,16 @@
 async function moduleProject4() {
 
   // 👇 WORK WORK BELOW THIS LINE 👇
+  // 
   const footer = document.querySelector('footer')
   const currentYear = new Date().getFullYear()
   footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
 
+
+
   let descriptions = [
-    ["Sunny", "☀️"],
-    ["Cloudy", "☁️"],
+    ["Sunny", "😃"],
+    ["Cloudy", "😵"],
     ["Rainy", "🌧️"],
     ["Thunderstorm", "⛈️"],
     ["Snowy", "❄️"],
@@ -15,6 +18,80 @@ async function moduleProject4() {
   ]
 
   // 👉 Tasks 1 - 5 go here
+  document.querySelector('#weatherWidget').style.display ='none'
+  document.querySelector('#citySelect').addEventListener('change',async evt => {
+  console.log('action changed')
+  
+   try {
+    document.querySelector('#citySelect').setAttribute('disabled', 'disabled')
+    document.querySelector('#weatherWidget').style.display ='none'
+    document.querySelector('.info').textContent ='fetching weather data...'
+
+    console.log(evt.target.value)
+    let city = evt.target.value
+    let url = `   http://localhost:3003/api/weather?city=${city}`
+    console.log(url)
+
+
+    
+    const res = await axios.get(url)
+
+    document.querySelector('#weatherWidget').style.display ='block'
+    document.querySelector('.info').textContent = ''
+    evt.target.removeAttribute('disabled')
+
+    let { data } = res
+    console.log()
+    document.querySelector('#apparentTemp div:nth-child(2)')
+      .textContent = `${data.current.apparent_temperature}°`
+
+      document.querySelector('#todayDescription')
+      .textContent = descriptions.find(d => d[0] === data.current.weather_description)[1]
+
+      document.querySelector('#todayStats div:nth-child(1)')
+      .textContent = `${data.current.temperature_min}°/${data.current.temperature_max}°`
+
+      document.querySelector('#todayStats div:nth-child(2)')
+      .textContent = ` Precipitation: ${data.current.precipitation_probability * 100}%`
+      console.log(data.current.precipitation_probability * 100)
+
+      document.querySelector('#todayStats div:nth-child(3)')
+      .textContent = ` Humidity: ${data.current.humidity}%`
+
+      document.querySelector('#todayStats div:nth-child(4)')
+      .textContent = `Wind: ${data.current.wind_speed}m/s`
+
+      function getDayOfWeek(date) {
+        const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        const dayIndex = date.getDay();
+        return daysOfWeek[dayIndex];
+      }
+      
+      data.forecast.daily.forEach((day, idx) => {
+        let card = document.querySelectorAll('.next-day')[idx];
+      
+        let weekDay = card.children[0];
+        let apparent = card.children[1];
+        let minMax = card.children[2];
+        let precipit = card.children[3];
+      
+        const date = new Date(day.date); // Convert the date string to a Date object
+        weekDay.textContent = getDayOfWeek(date); // Set the day of the week
+      
+        apparent.textContent = descriptions.find(d => d[0] === day.weather_description)[1];
+        minMax.textContent = `${day.temperature_min}°/${day.temperature_max}°`;
+        precipit.textContent = `Precipitation: ${day.precipitation_probability * 100}%`;
+      });
+      
+      document.querySelector('#location').firstElementChild.textContent = data.location.city;
+      
+    
+   } catch (err) {
+    console.log(`Promis rejected an error.message --> `, err.message)
+   }
+   })
+
+
 
   // 👆 WORK WORK ABOVE THIS LINE 👆
 
